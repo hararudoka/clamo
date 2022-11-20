b75e1cb9fc16e994fe80cfb94884374fe5565b9b
hararudoka <44776391+hararudoka@users.noreply.github.com>
2022-11-20 11:19:48 +0000

    removed useless interface

--- server/nethttp/handler.go
+++ server/nethttp/handler.go
@@ -9,18 +9,15 @@ import (
 	"github.com/hararudoka/clamo/server/service"
 )
 
-// Service is a interface (list of requested methods) for service layer. TODO: implement this
-type Service interface{}
-
 // Handler is a struct for http handlers, which contains Service
 type Handler struct {
-	Service
+	service.Service
 }
 
 // New creates new Handler
 func New(s service.Service) http.Handler {
 	return Handler{
-		&s,
+		s,
 	}
 }
 
